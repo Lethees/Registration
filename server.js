210efa72436f6e49029d5684a605b3790b40d89c
lethees <514180089@qq.com>
2020-04-05 03:22:59 -0600

    updates

--- server.js
+++ server.js
@@ -10,10 +10,6 @@ app.set('port', (process.env.PORT || 5000))
 
 .use(express.static(__dirname + '/public'))
 
-.set('views', __dirname + '/views')
-  
-.set('view engine', 'ejs')
-
 .get("/", (req, res) => {
     res.sendFile("form.php", { root: __dirname + "/public"});
  });
